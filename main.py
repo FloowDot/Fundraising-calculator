75f20af87a97409d596f11b10ef0a45bdcc08b48
FloowDot <eli.chandler@gmail.com>
2020-03-16 23:07:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#Not blank function goes here
+def noAlpha(question):
+  while True:
+    response = input(question)
+    for letter in response:
+      if letter.isalpha():
+        continue
+      else:
+        if response == "":
+          continue
+        else:
+          return response
+
+hasErrors = False
+
+#Get user profit goal
+profitGoal = noAlpha("Please enter the profit goal for your compaign: ")
+#Make sure profit goal is a number
+
+
+
+#Error checking
+
+
+
