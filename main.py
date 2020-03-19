5361bb8831bf5065c665694e0c3b39a22ad171dd
FloowDot <eli.chandler@gmail.com>
2020-03-19 20:47:03 +0000

    added product listing function

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
 import csv
 #Not blank function goes here
-
+#make sure the variables that need to be numbers dont have letters
 def noLetters (question):
   valid = False
   while not valid:
@@ -15,7 +14,16 @@ def noLetters (question):
 
 
 #Get user profit goal
-profitGoal = noLetters("Please enter the profit goal for your compaign: ")
+profitGoal = noLetters("Please enter the profit goal for your campaign: ")
 #Make sure profit goal is a number
 
 print(profitGoal)
+
+productList = []
+valid = False
+while not valid:
+  product = input("Enter product name, enter  xx to finish list:")
+  productList.append(product)
+  if product == "xx":
+    print("Heres your list: ", productList)
+    break
